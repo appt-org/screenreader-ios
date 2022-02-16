71eb1b1021a1cc87c63ff7cdbdc3364cf593bcbf
Jan Jaap de Groot <info@janjaap.com>
2022-02-16 13:41:38 +0100

    Initial Commit
    Initial commit

---
+++ Screenreader/Test.swift
@@ -0,0 +1,9 @@
+//
+//  Test.swift
+//  Screenreader
+//
+//  Created by Jan Jaap de Groot on 16/02/2022.
+//  Copyright © 2022 Abra B.V. / Stichting Appt. All rights reserved.
+//
+
+import Foundation
---
+++ Screenreader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  screenreader
+//
+//  Created by Jan Jaap de Groot on 16/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
